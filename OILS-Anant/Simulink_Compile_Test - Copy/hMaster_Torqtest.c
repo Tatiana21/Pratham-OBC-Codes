d44d70f898cf5bb566ae41b1363b75872b223dbf
anantjoshi97 <anantjoshi97.iitb@gmail.com>
2017-02-03 01:10:44 +0530

    Compiling code with simulink
    Code is exchanging data with matlab and simulink model in matlab is
    compiling successfully, haven't tested matlab model for correctness yet.

--- OILS-Anant/Simulink_Compile_Test - Copy/hMaster_Torqtest.c
+++ OILS-Anant/Simulink_Compile_Test - Copy/hMaster_Torqtest.c
@@ -45,38 +45,73 @@ int main(void)
  
 	configure_torquer();
 	
-	int size_mag = 0;
-	int size_sun = 0;
-	int size_gps = 0;
+	int size_mag = 6;
+	int size_sun = 12;
+	int size_gps = 155;
 	uint8_t mag[size_mag];
 	uint8_t sun[size_sun];
 	uint8_t gps[size_gps];
+		for(int i=0; i<size_gps; i++){
+			gps[i] = 0;
+		}
+		
+		for(int i=0; i<size_mag; i++){
+			mag[i] = 0;
+		}
+		
+		for(int i=0; i<size_sun; i++){
+			sun[i] = 0;
+		}
 	
 
 	while(1){              
-	PORTA = 0xA0;
-	_delay_ms(1000);
-	PORTA = 0x50;
-	//_delay_ms(500);
-	_delay_ms(1000);
 	
-
-	
+		
 	init_UART0();
+	int count = 0;
+	
+	for(int i=0; i<size_gps; i++){
+		gps[i] = receive_UART0();
+		count = count + 1;
+	}
 	
 	for(int i=0; i<size_mag; i++){
 		mag[i] = receive_UART0();
+		count = count + 1;
 	}
 	
 	for(int i=0; i<size_sun; i++){
 		sun[i] = receive_UART0();
+		count = count + 1;
 	}
 	
-	for(int i=0; i<size_gps; i++){
-		gps[i] = receive_UART0();
+	
+	for(int iter=0; iter<2; iter++){
+		PORTA = 0xA0;
+		_delay_ms(1000);
+		PORTA = 0x50;
+		//_delay_ms(500);
+		_delay_ms(1000);
+	}	
+	
+	init_UART0();
+	
+	uint8_t send_digit = 1;
+	if(count == 173){
+	for(int iter=0; iter<6; iter++){
+		uint8_t send_digit = iter;
+		transmit_UART0(send_digit);
+		_delay_ms(1);
+	}
 	}
 	
-	transmit_UART0((uint8_t)0);
+	for(int iter=0; iter<2; iter++){
+		PORTA = 0xA0;
+		_delay_ms(500);
+		PORTA = 0x50;
+		//_delay_ms(500);
+		_delay_ms(500);
+	}
 
 	}
 	return 0;
